61f2e9388449d7cc14712a0564c6b91eba424aa3
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-05-31 16:04:21 +0200

    upgrade design + clean files

--- src/components/admin/ChoicePageAdmin.jsx
+++ src/components/admin/ChoicePageAdmin.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const ChoicePageAdmin = ({ setChoice }) => {
   return (
-    <div className='bg-principal-color flex flex-col gap-8 items-center justify-center py-6 text-white text-xl lg:flex-row lg:gap-32 2xl:rounded-b-xl'>
+    <div className='bg-lime-800  flex flex-col gap-8 items-center justify-center py-6 text-white text-xl lg:flex-row lg:gap-32 2xl:rounded-b-xl'>
       <a
         className='flex transform transition duration-500 hover:scale-125 cursor-pointer origin-center'
         onClick={() => setChoice('equestrianCenter')}
