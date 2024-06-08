97c1ff211a57847238870d78a0f8e0a0d82a9fda
Segolene GANZIN <segolene.ganzin@gmail.com>
2024-06-08 08:08:10 +0200

    modal z index

--- src/layouts/Modal.jsx
+++ src/layouts/Modal.jsx
@@ -28,7 +28,7 @@ const Modal = ({ isModalOpen, setModalOpen, title, children }) => {
           <span className='absolute inset-0 bg-gray-500 opacity-75'></span>
         </div>
 
-        <div className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full'>
+        <div className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full z-50'>
           <div className='bg-white px-4 pt-5 pb-0 sm:p-6 sm:pb-4'>
             <button
               className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:h-10 sm:w-10 sm:absolute sm:right-4 sm:top-4 z-50'
